a8fe51a7203023c58131a8776de47211126d5ba5
Cody Jennings <codyjennings95@gmail.com>
2025-07-11 15:05:34 -0400

    Fixed importing
    Redid with better coding practices

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-import { mapInfo } from "./map.js";
+import { Map } from "./map.js";
+import { Tile, Floor, Wall } from "./tile.js";
 
 
-var displayOptions = {
+
+
+function run()
+{
+	const displayOptions = {
 	width: 50,
 	height: 50,
 	fontSize: 24,
@@ -9,10 +14,53 @@ var displayOptions = {
 	forceSquareRatio: true
 	
 }
-var display = new ROT.Display(displayOptions);
+
+const display = new ROT.Display(displayOptions);
+const w = 50; //width of the map
+const h = 50; //height of the map
+
+const mapInfo = new Map(w, h, initTiles(w, h));
+
+
+const digger = new ROT.Map.Digger(w,h);   //create a digger map object
+
+let userCallback = function(x, y, value) {
+    if (value == 0){
+        mapInfo.tiles[y][x] = new Floor(x, y);
+    }
+    else if (value == 1){
+        mapInfo.tiles[y][x] = new Wall(x, y);
+    }
+
+}
+function displayMap(map, display){
+    for(let row = 0; row < map.mapWidth; row++){
+        for(let column = 0; column < map.mapHeight; column++){
+            display.draw(mapInfo.tiles[row][column].x, mapInfo.tiles[row][column].y, mapInfo.tiles[row][column].sprite);
+        }
+    }
+}
+function initTiles(mw, mh){  //creates a 2d array made up of Tiles
+        const tiles = [];
+
+        for(let row = 0; row < mw; row++){
+            tiles[row] = [];
+            for(let column = 0; column < mh; column++){
+                tiles[row][column] = new Tile(column, row);
+            }
+        }
+
+        return tiles;
+    }
+digger.create(userCallback);
+displayMap(mapInfo, display);
 document.body.appendChild(display.getContainer());
 
-display.draw(5,4,"@");
-export {display}
+}
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', run);
 
--- map.js
+++ map.js
@@ -1,59 +1,15 @@
-import { display } from "./main.js";
-import { Tile, Floor, Wall } from "./tile.js";
 
 
-let w = 40; //width of the map
-let h = 25; //height of the map
+
+
 
 class Map{          //a class that contains the width and height of the map, represented by tiles
-    constructor(mapWidth, mapHeight){
+    constructor(mapWidth, mapHeight, tiles){
         this.mapWidth = mapWidth;
         this.mapHeight = mapHeight;
-        
-        
-    }
-
-    initTiles(mw, mh){  //creates a 2d array made up of Tiles
-        const tiles = [];
-
-        for(let row = 0; row < mw; row++){
-            tiles[row] = [];
-            for(let column = 0; column < mh; column++){
-                tiles[row][column] = new Tile(column, row);
-            }
-        }
-
         this.tiles = tiles;
+        
     }
 }
 
-let mapInfo = new Map(w, h);
-mapInfo.initTiles(w, h);
-
-let starterRotMap = new ROT.Map.Digger(w,h);   //create a digger map object
-
-
-
-let userCallback = function(x, y, value) {
-    if (value == 0){
-        mapInfo.tiles[y][x] = new Floor(x, y);
-    }
-    else if (value == 1){
-        mapInfo.tiles[y][x] = new Wall(x, y);
-    }
-    
-    
-}
-starterRotMap.create(userCallback);
-console.log(mapInfo.tiles);
-//displayMap(mapInfo, display);
-
-function displayMap(map, display){
-    for(let row = 0; row < map.mapWidth; row++){
-        for(let column = 0; column < map.mapHeight; column++){
-            display.draw(mapInfo.tiles[row][column].x, mapInfo.tiles[row][column].y, mapInfo.tiles[row][column].sprite);
-        }
-    }
-}
-
-export {mapInfo}
+export {Map};
